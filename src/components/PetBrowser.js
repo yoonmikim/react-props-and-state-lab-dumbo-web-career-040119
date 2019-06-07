430fc879145e6ac758e92c45019e0639cbd88665
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-07 15:09:16 +0000

    Automatically backed up by Learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,10 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">PET COMPONENT SHOULD GO HERE
+      <Pet />
+    </div>
+    
   }
 }
 
